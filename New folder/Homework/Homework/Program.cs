05aaa0e6ef85144c1cc93da7b630a6e13505817b
Agneta Gustafsson <agneta.kode@outlook.com>
2018-01-10 13:16:43 +0100

    empty 3

--- New folder/Homework/Homework/Program.cs
+++ New folder/Homework/Homework/Program.cs
@@ -14,15 +14,10 @@ namespace Homework
         static void Main(string[] prefixes)
         {
             FileReader myFileReader = new FileReader();
-            string index = "";
-            foreach(string line in myFileReader.Index)
-            {
-                index += line;
-            }
-            try
-            {
 
+          
                 if (!HttpListener.IsSupported)
+          
                 {
                     Console.WriteLine("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
                     return;
@@ -30,55 +25,38 @@ namespace Homework
                 // URI prefixes are required,
                 // for example "http://contoso.com:8080/index/".
                 if (prefixes == null || prefixes.Length == 0)
-                {
                     throw new ArgumentException("prefixes");
 
-                }
-                while(listener.IsListening)
+                // Create a listener.
+                HttpListener listener = new HttpListener();
+                // Add the prefixes.
+                foreach (string s in prefixes)
                 {
-                    // Add the prefixes.
-                    foreach (string s in prefixes)
-                    {
-                        listener.Prefixes.Add(s);
-                    }
-                    listener.Start();
-                    Console.WriteLine("Listening...try again if you want.");
-                    // Note: The GetContext method blocks while waiting for a request. 
-                    HttpListenerContext context = listener.GetContext();
-                    HttpListenerRequest request = context.Request;
-                    // Obtain a response object.
-                    HttpListenerResponse response = context.Response;
-                    // Construct a response.
-
-                    string test = "";
-                    foreach (string line in context.Request.Headers.AllKeys)
-                    {
-                        test += line;
-                    }
-
-
-                    string responseString = test;
-                    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
-
-                    // Get a response stream and write the response to it.
-            
-                    response.ContentLength64 = buffer.Length;
-            
-                    System.IO.Stream output = response.OutputStream;
-                    output.Write(buffer, 0, buffer.Length);
-            
-                    // You must close the output stream.
-                    output.Close();
-                    listener.Stop();
-
-                    //useful: Environment.CurrentDirectory
+                    listener.Prefixes.Add(s);
                 }
-            }
-            catch(WebException e)
-            {
-                Console.WriteLine(e.Status);
-            }
+                listener.Start();
+                Console.WriteLine("Listening...try again if you want.");
+                // Note: The GetContext method blocks while waiting for a request. 
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+                // Obtain a response object.
+                HttpListenerResponse response = context.Response;
+                // Construct a response.
+                string responseString = myFileReader.Index;
+
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                // Get a response stream and write the response to it.
+                response.ContentLength64 = buffer.Length;
+                System.IO.Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                // You must close the output stream.
+                output.Close();
+                listener.Stop();
+
+        
         }
 
     }
 }
+
+
