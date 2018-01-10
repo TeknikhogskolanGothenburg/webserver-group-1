0d53b092b4d9ed51ae3bbde83a45c9cd6aa1ee63
Agneta Gustafsson <agneta.kode@outlook.com>
2018-01-10 14:17:19 +0100

    .

--- New folder/Homework/Homework/FileReader.cs
+++ New folder/Homework/Homework/FileReader.cs
@@ -10,9 +10,14 @@ namespace Homework
     public class FileReader
     {
         public string Index { get; set; }
+
+
         public FileReader()
         {
+            
+           
             Index = File.ReadAllText(Environment.CurrentDirectory + @"../../../../../../" + @"Content/index.html");
+         
         }
 
     }
