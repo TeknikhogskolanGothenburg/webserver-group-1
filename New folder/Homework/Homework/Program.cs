4769356cf27d8b742dda94b3a904f8b15f81fe86
Agneta Gustafsson <agneta.kode@outlook.com>
2018-01-10 11:04:09 +0100

    div

--- New folder/Homework/Homework/Program.cs
+++ New folder/Homework/Homework/Program.cs
@@ -4,52 +4,69 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
+using System.IO;
+
+
 
 namespace Homework
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-        }
+        
 
-        public static void SimpleListenerExample(string[] prefixes)
+        static void Main(string[] prefixes)
+
         {
             if (!HttpListener.IsSupported)
             {
                 Console.WriteLine("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
                 return;
             }
-            // URI prefixes are required,
-            // for example "http://contoso.com:8080/index/".
+
             if (prefixes == null || prefixes.Length == 0)
                 throw new ArgumentException("prefixes");
-
-            // Create a listener.
-            HttpListener listener = new HttpListener();
-            // Add the prefixes.
-            foreach (string s in prefixes)
+            try
             {
-                listener.Prefixes.Add(s);
+
+             while(true)
+                {
+                // Create a listener.
+                HttpListener listener = new HttpListener();
+                listener.Prefixes.Add("http://localhost:8080/");
+                listener.Start();
+
+                Console.WriteLine("Listening...try again if you want.");
+                // Note: The GetContext method blocks while waiting for a request. 
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+                // Obtain a response object.
+                HttpListenerResponse response = context.Response;
+                // Construct a response.
+
+                string responseString = "<HTML><BODY>hej Agneta</BODY></HTML>";
+                string test = request.RawUrl.
+
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                // Get a response stream and write the response to it.
+                response.ContentLength64 = buffer.Length;
+
+                Stream output = response.OutputStream;
+
+                output.Write(buffer, 0, buffer.Length);
+                // You must close the output stream.
+                output.Close();
+                listener.Stop();
+                }
+
+                
             }
-            listener.Start();
-            Console.WriteLine("Listening...");
-            // Note: The GetContext method blocks while waiting for a request. 
-            HttpListenerContext context = listener.GetContext();
-            HttpListenerRequest request = context.Request;
-            // Obtain a response object.
-            HttpListenerResponse response = context.Response;
-            // Construct a response.
-            string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
-            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
-            // Get a response stream and write the response to it.
-            response.ContentLength64 = buffer.Length;
-            System.IO.Stream output = response.OutputStream;
-            output.Write(buffer, 0, buffer.Length);
-            // You must close the output stream.
-            output.Close();
-            listener.Stop();
+            catch (WebException e)
+            {
+                Console.WriteLine(e.Status);
+            }
+
         }
+
 
     }
 }
