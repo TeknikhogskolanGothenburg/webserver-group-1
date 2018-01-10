040665ce9b8a3e672d4eb443c18894939e0fb4e3
matti <matti@LAPTOP-T1JAIQK0>
2018-01-10 10:35:08 +0100

    plop

--- New folder/Homework/Homework/Program.cs
+++ New folder/Homework/Homework/Program.cs
@@ -9,12 +9,8 @@ namespace Homework
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-        }
-
-        public static void SimpleListenerExample(string[] prefixes)
-        {
+        static void Main(string[] prefixes)
+        {       
             if (!HttpListener.IsSupported)
             {
                 Console.WriteLine("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
@@ -23,7 +19,10 @@ namespace Homework
             // URI prefixes are required,
             // for example "http://contoso.com:8080/index/".
             if (prefixes == null || prefixes.Length == 0)
+            {
                 throw new ArgumentException("prefixes");
+
+            }
 
             // Create a listener.
             HttpListener listener = new HttpListener();
@@ -38,14 +37,17 @@ namespace Homework
             HttpListenerContext context = listener.GetContext();
             HttpListenerRequest request = context.Request;
             // Obtain a response object.
-            HttpListenerResponse response = context.Response;
+            HttpListenerResponse response = context.Response;            
             // Construct a response.
-            string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+            
+            string responseString = "<HTML><BODY> T.T </BODY></HTML>";
             byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            
             // Get a response stream and write the response to it.
             response.ContentLength64 = buffer.Length;
             System.IO.Stream output = response.OutputStream;
             output.Write(buffer, 0, buffer.Length);
+            
             // You must close the output stream.
             output.Close();
             listener.Stop();
