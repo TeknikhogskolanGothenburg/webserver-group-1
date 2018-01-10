b0949fdc4a0650b600852075d74938cc2f44ca26
matti <matti@LAPTOP-T1JAIQK0>
2018-01-10 12:02:06 +0100

    indent fixes

--- New folder/Homework/Homework/Program.cs
+++ New folder/Homework/Homework/Program.cs
@@ -20,53 +20,60 @@ namespace Homework
             try
             {
 
-            if (!HttpListener.IsSupported)
-            {
-                Console.WriteLine("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
-                return;
-            }
-            // URI prefixes are required,
-            // for example "http://contoso.com:8080/index/".
-            if (prefixes == null || prefixes.Length == 0)
-            {
-                throw new ArgumentException("prefixes");
+                if (!HttpListener.IsSupported)
+                {
+                    Console.WriteLine("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
+                    return;
+                }
+                // URI prefixes are required,
+                // for example "http://contoso.com:8080/index/".
+                if (prefixes == null || prefixes.Length == 0)
+                {
+                    throw new ArgumentException("prefixes");
 
-            }
-            while(true)
-            {
+                }
+                while(true)
+                {
 
-            // Create a listener.
-            HttpListener listener = new HttpListener();
-            // Add the prefixes.
-            foreach (string s in prefixes)
-            {
-                listener.Prefixes.Add(s);
-            }
-            listener.Start();
-            Console.WriteLine("Listening...try again if you want.");
-            // Note: The GetContext method blocks while waiting for a request. 
-            HttpListenerContext context = listener.GetContext();
-            HttpListenerRequest request = context.Request;
-            // Obtain a response object.
-            HttpListenerResponse response = context.Response;
-            // Construct a response.
-            
-            string responseString = index;
-            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                    // Create a listener.
+                    HttpListener listener = new HttpListener();
+                    // Add the prefixes.
+                    foreach (string s in prefixes)
+                    {
+                        listener.Prefixes.Add(s);
+                    }
+                    listener.Start();
+                    Console.WriteLine("Listening...try again if you want.");
+                    // Note: The GetContext method blocks while waiting for a request. 
+                    HttpListenerContext context = listener.GetContext();
+                    HttpListenerRequest request = context.Request;
+                    // Obtain a response object.
+                    HttpListenerResponse response = context.Response;
+                    // Construct a response.
 
-            // Get a response stream and write the response to it.
-            
-            response.ContentLength64 = buffer.Length;
-            
-            System.IO.Stream output = response.OutputStream;
-            output.Write(buffer, 0, buffer.Length);
-            
-            // You must close the output stream.
-            output.Close();
-            listener.Stop();
+                    string test = "";
+                    foreach (string line in context.Request.Headers.AllKeys)
+                    {
+                        test += line;
+                    }
 
-            //useful: Environment.CurrentDirectory
-            }
+
+                    string responseString = test;
+                    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+
+                    // Get a response stream and write the response to it.
+            
+                    response.ContentLength64 = buffer.Length;
+            
+                    System.IO.Stream output = response.OutputStream;
+                    output.Write(buffer, 0, buffer.Length);
+            
+                    // You must close the output stream.
+                    output.Close();
+                    listener.Stop();
+
+                    //useful: Environment.CurrentDirectory
+                }
             }
             catch(WebException e)
             {
