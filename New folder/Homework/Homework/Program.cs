ddc8797a1aad3d563c7fe4c6e1b0289d511a3224
matti <matti@LAPTOP-T1JAIQK0>
2018-01-11 12:48:56 +0100

    cleaner code, and resource paths is now a dictionary

--- New folder/Homework/Homework/Program.cs
+++ New folder/Homework/Homework/Program.cs
@@ -14,12 +14,13 @@ namespace Homework
         public static HttpListener listener = new HttpListener();
 
         static void Main(string[] prefixes)
-        {
+        {            
             FileReader myFileReader = new FileReader();
             SiteResources a = new SiteResources();
             
             try
             {
+                            
 
                 if (!HttpListener.IsSupported)
                 {
@@ -29,19 +30,22 @@ namespace Homework
                 // URI prefixes are required,
                 // for example "http://contoso.com:8080/index/".
                 // Add the prefixes.
-                foreach (string s in prefixes)
-                {
-                    listener.Prefixes.Add(s);
-                }
                 while(true)
                 {
                     listener.Start();
+                    foreach (string s in prefixes)
+                    {
+                        listener.Prefixes.Add(s);
+                    }
                     if (prefixes == null || prefixes.Length == 0)
                     {
                         throw new ArgumentException("prefixes");
 
                     }
                     Console.WriteLine("Listening...try again if you want.");
+
+
+
                     // Note: The GetContext method blocks while waiting for a request. 
                     HttpListenerContext context = listener.GetContext();
                     HttpListenerRequest request = context.Request;
@@ -50,40 +54,22 @@ namespace Homework
                     // Construct a response.
                     SiteResources FilesOnServer = new SiteResources();
 
+
                     byte[] buffer = new byte[] { };
-                       
-                    context.Response.ContentType = MimeMapping.GetMimeMapping(FilesOnServer.FilePaths[2]);
-                    
-                    string end = "";
                     Console.WriteLine(request.RawUrl);
-                    if(request.RawUrl == "/pianocat.gif")
-                    {
-                        
-                        context.Response.ContentType = MimeMapping.GetMimeMapping(FilesOnServer.FilePaths[4]);
-                        Console.WriteLine(context.Response.ContentType);
-                    }
-                    if (request.RawUrl == "/" || request.RawUrl == "/Subfolder/")
-                    {
-                        end = "index.html";
-                    }
-                    if (request.RawUrl != "/favicon.ico")
-                    {
-                        string test = File.ReadAllText(Environment.CurrentDirectory + "../../../../../../\\Content" + request.RawUrl + end);
-
-                        buffer = Encoding.UTF8.GetBytes(test);
-                        // Get a response stream and write the response to it.
-
-                        response.ContentLength64 = buffer.Length;
-
-                        Stream output = response.OutputStream;
-                        output.Write(buffer, 0, buffer.Length);
-                        // You must close the output stream.
-                        output.Close();
-                    }
                     
-                    listener.Stop();
+                    //response.ContentType = EnAvFunctionerna(request.RawUrl);
+                    //buffer = DenAndraFunctionen(request.RawUrl);
+                    //MimeMapping.GetMimeMapping;
+                    //File.ReadAllBytes;                    
 
-                    //useful: Environment.CurrentDirectory
+                    // Get a response stream and write the response to it.
+                    response.ContentLength64 = buffer.Length;
+                    Stream output = response.OutputStream;
+                    output.Write(buffer, 0, buffer.Length);
+                    // You must close the output stream.
+                    output.Close();                    
+                    listener.Stop();
                 }
             }
             catch(WebException e)
@@ -91,6 +77,5 @@ namespace Homework
                 Console.WriteLine(e.Status);
             }
         }
-
     }
 }
--- New folder/Homework/Homework/SiteResources.cs
+++ New folder/Homework/Homework/SiteResources.cs
@@ -12,13 +12,11 @@ namespace Homework
     class SiteResources
     {
         public string Root = Environment.CurrentDirectory + "../../../../../../" + "Content";
-        public List<string> FilePaths = new List<string>();
+        public Dictionary<string, string> FilePaths { get; set; }
         public SiteResources()
         {
-            FilePaths = FindAllFiles(Root);
- 
+            FilePaths = ConvertToDic(FindAllFiles(Root));            
         }
-
 
         //returns the path of each file in a given directory and all its subfolders
         public List<string> FindAllFiles(string path)
@@ -39,6 +37,19 @@ namespace Homework
                 }                
             }
             return result;
-        }        
+        }
+
+        private Dictionary<string,string> ConvertToDic(List<string> list)
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+
+            foreach (string path in list)
+            {
+                string trash = (Environment.CurrentDirectory + "../../../../../../" + "Content");
+                string key = path.Substring(trash.Length, path.Length - trash.Length);
+                result.Add(key, path);
+            }
+            return result;
+        }
     }
 }
